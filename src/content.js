51da6d8b28af22c9af3cd135a9ff11b13ca4a44d
Heba1998 <hebaalmomani1998@gmail.com>
2021-08-09 15:28:30 +0300

    comment

--- src/content.js
+++ src/content.js
@@ -1,61 +1,61 @@
-import React, { Component } from 'react';
-import { withAuth0 } from '@auth0/auth0-react';
-import axios from 'axios';
+// import React, { Component } from 'react';
+// import { withAuth0 } from '@auth0/auth0-react';
+// import axios from 'axios';
 
-export class Content extends Component {
+// export class Content extends Component {
 
-  constructor(props) {
-    super(props);
+//   constructor(props) {
+//     super(props);
 
-    this.state = {
-      user: {}
-    }
-  }
+//     this.state = {
+//       user: {}
+//     }
+//   }
 
-  componentDidMount = () => {
-    this.props.auth0.getIdTokenClaims().then(tokenResponse => {
+//   componentDidMount = () => {
+//     this.props.auth0.getIdTokenClaims().then(tokenResponse => {
 
-      const jwt = tokenResponse.nickname;
-      console.log(tokenResponse);
+//       const jwt = tokenResponse.nickname;
+//       console.log(tokenResponse);
 
 
-      const config = {
-        headers: {
-          "Authorization": `Bearer ${jwt}`
-        },
-        method: 'get',
-        baseURL: process.env.REACT_APP_SERVER,
-        url: '/verify-token'
-      };
+//       const config = {
+//         headers: {
+//           "Authorization": `Bearer ${jwt}`
+//         },
+//         method: 'get',
+//         baseURL: process.env.REACT_APP_SERVER,
+//         url: '/verify-token'
+//       };
 
-      axios(config).then((axiosResponse) => {
-        console.log('backend data', axiosResponse.data);
+//       axios(config).then((axiosResponse) => {
+//         console.log('backend data', axiosResponse.data);
 
-        this.setState({
-          user: axiosResponse.data
-        })
-      }).catch(error => alert(error))
+//         this.setState({
+//           user: axiosResponse.data
+//         })
+//       }).catch(error => alert(error))
 
-    }).catch(error => console.log(error));
-  };
+//     }).catch(error => console.log(error));
+//   };
 
-  render() {
-    return (
-      <div>
-        <p>
-          backend part
-        </p>
-        {
-          this.state.user.nickname &&
-          <p>
-            Data from Backend: {
-              this.state.user.nickname
-            }
-          </p>
-        }
-      </div>
-    )
-  }
-}
+//   render() {
+//     return (
+//       <div>
+//         <p>
+//           backend part
+//         </p>
+//         {
+//           this.state.user.nickname &&
+//           <p>
+//             Data from Backend: {
+//               this.state.user.nickname
+//             }
+//           </p>
+//         }
+//       </div>
+//     )
+//   }
+// }
 
-export default withAuth0(Content);
+// export default withAuth0(Content);
